8b9be308ebb2e334afcf56b748c1bba740c7110d
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-01-31 16:43:20 -0200

    change identation

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -20,25 +20,25 @@ export let create = (req: Request, res: Response) => {
         input: fs.createReadStream('/tmp/upload.csv'),
         output: process.stdout,
         terminal: false
-      });
+    });
 
-      var test = new Test();
+    var test = new Test();
 
-      rd.on('line', line =>  {
-          test.addMessage(Message.instance(line));
-      });
+    rd.on('line', line =>  {
+        test.addMessage(Message.instance(line));
+    });
 
-      rd.on('close', () => {
-        res.json({
-            device_ids: test.messages.map(message => message.deviceTimestamp.toString()),
-            delays: test.messages.map(message => message.delay),
-            delay_avarage: test.delayAvarage,
-            standard_derivation: test.standardDerivation,
-            out_of_order_messages: test.outOfOrderMessages,
-            total_received_messages: test.totalMessages,
-            total_sent_messages: test.totalSentMessages
-        });
-      });
+    rd.on('close', () => {
+    res.json({
+        device_ids: test.messages.map(message => message.deviceTimestamp.toString()),
+        delays: test.messages.map(message => message.delay),
+        delay_avarage: test.delayAvarage,
+        standard_derivation: test.standardDerivation,
+        out_of_order_messages: test.outOfOrderMessages,
+        total_received_messages: test.totalMessages,
+        total_sent_messages: test.totalSentMessages
+    });
+    });
     
   
 };
