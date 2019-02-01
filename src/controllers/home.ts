6aadbdfeea6fa4a92ad92b8fa81d992e78fd93ab
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-01 11:24:55 -0200

    layout adjustment

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,18 +1,11 @@
 import { Request, Response } from "express";
-import { Chart } from "../models/Chart";
 
 /**
  * GET /
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  let chart = new Chart();
-  chart.labels = ["a", "b", "c"]
-  chart.data = [1, 2, 3];
-
   res.render("home", {
     title: "Home",
-    labels: [],
-    data: []
   });
 };
