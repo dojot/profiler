7c782f8a81a01b7784055462fe9794c5211e12a3
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-01 10:26:52 -0200

    refactoring ui and controller

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,44 +1,44 @@
 import { Request, Response } from "express";
-import { Chart } from "../models/Chart";
 import { Test } from "../models/Test";
 import { Message } from "../models/Message";
 import fs = require('fs');
 import readline = require('readline');
+import { UploadedFile } from "express-fileupload";
 
 export let create = (req: Request, res: Response) => {
     if (Object.keys(req.files).length == 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    req.files.sampleFile.mv('/tmp/upload.csv', (err: any) => {
-        if (err) {
-            console.log('Error while copying file to target location');
-        }
-    });
-
-    var rd = readline.createInterface({
-        input: fs.createReadStream('/tmp/upload.csv'),
-        output: process.stdout,
-        terminal: false
-    });
+    if('mv' in req.files.sampleFile){
+        let sampleFile = req.files.sampleFile;
+        sampleFile.mv('/tmp/upload.csv', (err: any) => {
+            if (err) {
+                console.log('Error while copying file to target location');
+            }
+        });
+    }
 
     var test = new Test();
 
-    rd.on('line', line =>  {
+    readline.createInterface({
+        input: fs.createReadStream('/tmp/upload.csv'),
+        output: process.stdout,
+        terminal: false
+    })
+    .on('line', line =>  {
         test.addMessage(Message.instance(line));
-    });
-
-    rd.on('close', () => {
-    res.json({
-        device_ids: test.messages.map(message => message.deviceTimestamp.toString()),
-        delays: test.messages.map(message => message.delay),
-        delay_avarage: test.delayAvarage,
-        standard_derivation: test.standardDerivation,
-        out_of_order_messages: test.outOfOrderMessages,
-        total_received_messages: test.totalMessages,
-        total_sent_messages: test.totalSentMessages
-    });
-    });
-    
+    })
+    .on('close', () => {
+        res.json({
+            device_ids: test.messages.map(message => message.deviceTimestamp.toString()),
+            delays: test.messages.map(message => message.delay),
+            delay_avarage: test.delayAvarage,
+            standard_derivation: test.standardDerivation,
+            out_of_order_messages: test.outOfOrderMessages,
+            total_received_messages: test.totalMessages,
+            total_sent_messages: test.totalSentMessages
+        });
+    })    
   
 };
