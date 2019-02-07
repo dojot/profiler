5bcdb1f0c5eafeb46c64c2ad1b1125e3f7494b0e
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-07 14:28:40 -0200

    changing port number

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ import * as fileController from "./controllers/file";
 
 const app = express();
 
-app.set("port", process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.engine("html", ejs.renderFile);
 app.set("view engine", "html");
