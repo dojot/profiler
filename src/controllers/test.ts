b5e69267c6cc193c59322c9d814c7d96f454ce73
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-08 10:39:16 -0200

    formatting code

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -7,7 +7,6 @@ import util = require("util");
 import { ResultFile } from "../models/ResultFile";
 import { File } from "../models/File";
 import request = require("request");
-import querystring = require("querystring");
 import { SocketIoClient } from "../models/SocketioClient";
 import { FileLine } from "../models/FileLine";
 
@@ -22,86 +21,96 @@ export let create = (req: Request, res: Response) => {
 
   let data: any = [];
 
-
-
   try {
-    request.post(`http://${server}:8000/auth`, {
-      json: {
-        username: username,
-        passwd: password
-      }
-    },(er, rs, bd) => {
-      if (er) {
-        console.error(er)
-        return
-      }
-
-      request.get(`http://${server}:8000/stream/socketio`, {
-        headers: {
-          "Authorization": `Bearer ${bd.jwt}`
+    request.post(
+      `http://${server}:8000/auth`,
+      {
+        json: {
+          username: username,
+          passwd: password
         }
-      },(error, response, body) => {
-        if (error) {
-          console.error(error)
-          return
+      },
+      (er, rs, bd) => {
+        if (er) {
+          console.error(er);
+          return;
         }
 
-        const token = JSON.parse(body).token;
-
-        logger.debug(`Using server: ${server}, token: ${token}`);
-        let client = new SocketIoClient(`http://${server}:8000`, 1000, token.trim());
-
-        client.onMessage((data: any) => {
-          logger.debug(`Message received: ${util.inspect(data)}`);
-
-          const file = File.instance;
-          const message = FileLine.instance(data);
-
-          fs.appendFile(file.path, message.content, err => {
-            if (err) logger.debug(`Error writing message: ${util.inspect(data)}`);
-
-            if (message.last) {
-              logger.debug(`Last message: ${util.inspect(data)}`);
-
-              fs.rename(file.path, file.newPath, err => {
-                if (err) console.log("Error: " + err);
-              });
+        request.get(
+          `http://${server}:8000/stream/socketio`,
+          {
+            headers: {
+              Authorization: `Bearer ${bd.jwt}`
             }
-          });
-        });
+          },
+          (error, response, body) => {
+            if (error) {
+              console.error(error);
+              return;
+            }
 
-        client.start();
+            const token = JSON.parse(body).token;
 
-        shell.exec(
-          `mqtt-beamer ${server} ${tenant} ${device} ${perSecond} ${messages}`,
-          { async: true },
-          () => {
+            logger.debug(`Using server: ${server}, token: ${token}`);
+            const client = new SocketIoClient(
+              `http://${server}:8000`,
+              1000,
+              token.trim()
+            );
 
-            const watcher = fs.watch("/home/uploads", (eventType, filename) => {
-              if (filename != "result.csv") {
-                watcher.close();
-                client.close();
-                fs.readdir("/home/uploads", (err, files) => {
-                  data = files.map(f => {
-                    const result = new ResultFile(f);
-                    return {
-                      name: result.name,
-                      formattedName: result.formattedName
-                    };
+            client.onMessage((data: any) => {
+              logger.debug(`Message received: ${util.inspect(data)}`);
+
+              const file = File.instance;
+              const message = FileLine.instance(data);
+
+              fs.appendFile(file.path, message.content, err => {
+                if (err)
+                  logger.debug(`Error writing message: ${util.inspect(data)}`);
+
+                if (message.last) {
+                  logger.debug(`Last message: ${util.inspect(data)}`);
+
+                  fs.rename(file.path, file.newPath, err => {
+                    if (err) console.log("Error: " + err);
                   });
-                  res.json({
-                    files: data
-                  });
-                });
-              }
+                }
+              });
             });
+
+            client.start();
+
+            shell.exec(
+              `mqtt-beamer ${server} ${tenant} ${device} ${perSecond} ${messages}`,
+              { async: true },
+              () => {
+                const watcher = fs.watch(
+                  "/home/uploads",
+                  (eventType, filename) => {
+                    if (filename != "result.csv") {
+                      watcher.close();
+                      client.close();
+                      fs.readdir("/home/uploads", (err, files) => {
+                        data = files.map(f => {
+                          const result = new ResultFile(f);
+                          return {
+                            name: result.name,
+                            formattedName: result.formattedName
+                          };
+                        });
+                        res.json({
+                          files: data
+                        });
+                      });
+                    }
+                  }
+                );
+              }
+            );
           }
         );
-
-      });
-
-    });
-
+      }
+    );
   } catch (error) {
     console.log("Error: " + error);
   }
